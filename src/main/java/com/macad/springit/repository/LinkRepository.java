d90e5ba46a8579df56ee11f6e8340347362f5ec1
Jacob <jacobmcad@gmail.com>
2020-08-05 02:41:11 -0400

    Initial Repositories for Link and Comment

---
+++ src/main/java/com/macad/springit/repository/LinkRepository.java
@@ -0,0 +1,8 @@
+package com.macad.springit.repository;
+
+import com.macad.springit.domain.Link;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LinkRepository extends JpaRepository<Link, Long> {
+
+}
